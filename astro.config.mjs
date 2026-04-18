237868b6f469740e30a54a0d5b8eb84643462e56
hogdanish <121070110+hogdanish@users.noreply.github.com>
2026-04-17 19:34:47 -0500

    add GitHub Actions deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,9 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
-
-// https://astro.build/config
 export default defineConfig({
-    vite: {
+  output: 'static',
+  vite: {
     plugins: [tailwindcss()],
   },
 });
